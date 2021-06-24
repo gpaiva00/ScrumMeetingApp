553750aaaa820ff46d69a2fd98ada915b046184a
Gabriel Paiva <paivadepaiva00@gmail.com>
2021-06-24 16:51:49 -0300

    Initial Commit

---
+++ MyScrumdinger/MyScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyScrumdingerApp.swift
+//  MyScrumdinger
+//
+//  Created by Gabriel Paiva on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MyScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
